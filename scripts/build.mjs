4ff2d3bdff9338d91351de9ada6c775d93946548
adriatanaka-droid <adriatanaka-droid@users.noreply.github.com>
2026-08-09 11:48:07 +0900

    Add custom RH favicon

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -4,6 +4,7 @@ await mkdir("dist", { recursive: true });
 await copyFile("index.html", "dist/index.html");
 await copyFile("article.html", "dist/article.html");
 await copyFile("book.html", "dist/book.html");
+await copyFile("favicon.svg", "dist/favicon.svg");
 await copyFile("CNAME", "dist/CNAME");
 await copyFile(".nojekyll", "dist/.nojekyll");
 
